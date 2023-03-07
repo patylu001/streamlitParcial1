cf7536ca3892def254a52d9200e320bc0fc11ccc
patylu001 <patricia.ortegon@gmail.com>
2023-03-06 20:37:53 -0600

    adding azure model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-
+import pickle
 dfCalificaciones = pd.read_excel("tareas.xlsx")
 
 dfCalificaciones["promedio"] = ( dfCalificaciones["tarea1"] + dfCalificaciones["tarea2"] +
@@ -13,3 +13,29 @@ dfResultados = dfCalificaciones[ ["matricula","promedio","final"] ]
 dfResultados
 
 st.dataframe(dfResultados)
+
+model = pickle.load(open("model.pkl","rb"))
+
+data = {
+        "age": 17,
+        "job": "unemployed",
+        "marital": "single",
+        "education": "university.degree",
+        "default": "no",
+        "housing": "yes",
+        "loan": "no",
+        "contact": "cellular",
+        "month": "may",
+        "duration": 3000,
+        "campaign": 50,
+        "pdays": 500,
+        "previous": 2000,
+        "poutcome": "success",
+        "emp.var.rate": 3,
+        "cons.price.idx": 93,        
+        "nr.employed": 5000
+      }
+
+df = pd.json_normalize(data)
+
+st.write(model.predict(df))
